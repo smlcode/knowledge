7ad3fac80a43420abf5968c68a9edfcc2456b3cf
slee <steven.m.lee@afs.com>
2025-05-01 16:26:49 -0500

    nx angular mfe

---
+++ core/angular/nx/module-federation/angular-monorepo-mfe/apps/flow1/src/app/remote-entry/entry.routes.ts
@@ -0,0 +1,6 @@
+import { Route } from '@angular/router';
+import { RemoteEntryComponent } from './entry.component';
+
+export const remoteRoutes: Route[] = [
+  { path: 'flow-1', component: RemoteEntryComponent },
+];
